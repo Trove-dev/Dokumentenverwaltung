aec12bea6735769f8aca67cf0503e38e5c84117f
Katarina Minin <kminin@hh-berlin.de>
2021-07-11 20:01:12 +0200

    VS_Datum_DatumTest
    in Datum:
    Konstruktor, var jahr, monat, tag; Methoden: toString; aendereDatum (without body); getter; setter

---
+++ Dokumentenverwaltung/src/Datum/DatumTest.java
@@ -0,0 +1,12 @@
+package Datum;
+
+public class DatumTest {
+
+	public static void main(String[] args) {
+		
+		Datum date = new Datum (4,11,2020);
+		System.out.println(date.toString());
+
+	}
+
+}
