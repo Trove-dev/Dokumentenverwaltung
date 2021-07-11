9faf013febf4ca44133dd6efc3e77d41a853f8b3
Katarina Minin <kminin@hh-berlin.de>
2021-07-11 22:24:46 +0200

    VS_Papierkorb
    var, methoden

---
+++ Dokumentenverwaltung/src/papierkorb/Papierkorb.java
@@ -0,0 +1,20 @@
+package papierkorb;
+
+public class Papierkorb {
+	
+	private String name;
+	private double groesse;
+	
+	public Papierkorb() {
+		
+	}
+	
+	public void wiederherstelle(Datei datei) {
+		
+	}
+	
+	public void leerePapierkorb() {
+		
+	}
+	
+}
