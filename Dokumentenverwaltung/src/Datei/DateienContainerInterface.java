634f4bb2d32dc1540209925bb9341b5ee297b616
Trove-dev <mrlucas.schmitz@aol.de>
2021-07-13 22:10:24 +0200

    Edit to enable Merge

--- Dokumentenverwaltung/src/Datei/DateienContainerInterface.java
+++ Dokumentenverwaltung/src/Datei/DateienContainerInterface.java
@@ -2,7 +2,7 @@ package Datei;
 
 import java.nio.file.Path;
 
-public interface DateienContainerIF {
+public interface DateienContainerInterface {
 	public static Datei hochladeDatei(Path file, String name) {
 		return null;
 	}
